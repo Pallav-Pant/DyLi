64338819644c0c60e9d5ce6f53ddf51f91f1ddcc
Pallav <pant.30@osu.edu>
2021-10-08 17:46:35 -0400

    index on master: 6fe5a18 Transferred Class Data into a header file

--- DynamicList/DynamicList.cpp
+++ DynamicList/DynamicList.cpp
@@ -4,20 +4,11 @@
 
 int main() {
 	DynamicList<int> list;
+	list.addEnd(5);
 	list.addEnd(10);
-	list.addEnd(20);
-	list.addEnd(30);
-	list.addEnd(40);
-	list.addEnd(50);
-	list.addEnd(60);
-	list.addEnd(70);
 	list.addStart(0);
-	std::cout << list.at(-2) << std::endl;
-	list.removeEnd();
-	list.removeStart();
-	std::cout << list.at(0) << std::endl;
-	std::cout << list.at(-2) << std::endl;
-
+	list.removeat(1);
+	std::cout << list.at(1) << std::endl;
 }
 
 
